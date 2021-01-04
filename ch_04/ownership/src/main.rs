0bbac6a74adecf7ef1cc32f67931d8499b636362
bahodge <benjamin.andrew.hodge@gmail.com>
2021-01-03 22:08:58 -0600

    finished ch_15

--- ch_04/ownership/src/main.rs
+++ ch_04/ownership/src/main.rs
@@ -1,24 +1,24 @@
-fn main() {
-    let s = String::from("Hello");
+// fn main() {
+//     let s = String::from("Hello");
 
-    takes_ownership(s);
+//     takes_ownership(s);
 
-    //  // Should blow up
-    //  println!("s: {}", s);
+//     //  // Should blow up
+//     //  println!("s: {}", s);
 
-    let x = 5;
+//     let x = 5;
 
-    makes_copy(x);
-}
+//     makes_copy(x);
+// }
 
-fn takes_ownership(s: String) -> String {
-    println!("s: {}", s);
-    s
-}
+// fn takes_ownership(s: String) -> String {
+//     println!("s: {}", s);
+//     s
+// }
 
-fn makes_copy(x: i32) {
-    println!("x: {}", x);
-}
+// fn makes_copy(x: i32) {
+//     println!("x: {}", x);
+// }
 
 // fn main() {
 //     println!("Hello, world!");
@@ -39,3 +39,25 @@ fn makes_copy(x: i32) {
 // fn takes_and_gives_back(a_string: String) -> String {
 //     a_string
 // }
+
+fn main() {
+    // x is owned
+    let x = vec![1, 2, 3];
+    // push a bunch takes ownership
+    // x is now reassigned to the returned push_a_bunch
+    let x = push_a_bunch(x);
+
+    // x is now available again
+    println!("pushed a bunch {:?}", x)
+}
+
+fn push_a_bunch(mut v: Vec<u32>) -> Vec<u32> {
+    v.push(1);
+    v.push(1);
+    v.push(1);
+    v.push(1);
+    v.push(1);
+    v.push(1);
+    v.push(1);
+    v
+}
